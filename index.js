1e079d7d8d6d1e03533474f07aee60d10d5b812c
Rui Rodrigues <pv28292@alunos.estgv.ipv.pt>
2024-01-03 16:06:25 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const db = require("mongoose");
 const port = 3000;
+require('dotenv').config()
 
 const test = require('./routes/test')
 app.use(express.json());
 
 connectDB = async () => {
     try {
-      await db.connect('mongodb+srv://ruirodrigues:lWZHRWASAcDi5Ocv@uber.krhvbku.mongodb.net/', {
+      await db.connect(process.env.MONGOURL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
