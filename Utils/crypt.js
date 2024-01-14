7215791e660d9eba80f548cc1574cb2707517e05
Rui Rodrigues <pv28292@alunos.estgv.ipv.pt>
2024-01-14 20:52:21 +0000

    Delete Utils directory

--- Utils/crypt.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-module.exports = (uncrypted) => {
-    // Criptografa a senha
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(uncrypted, salt);
-    return hash;
-}
