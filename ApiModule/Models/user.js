0d2e987cfeb313c2cd30037bc4b61d5ebb2bb203
Rui Rodrigues <pv28292@alunos.estgv.ipv.pt>
2024-01-15 16:43:32 +0000

    User update

--- ApiModule/Models/user.js
+++ ApiModule/Models/user.js
@@ -1,55 +1,55 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    firstName: { 
-        type: String,
-        required: true 
-    },
-    lastName: { 
-        type: String,
-        required: true 
-    },
-    email: { 
-        type: String,
-        required: true 
-    },
-    phone: {
-        type: Number,
-        required: true
-    },
-    designation: {
-        type: String,
-        required: false,
-        default: null
-    },
-    birthday: {
-        type: Date,
-        required: false,
-        default: null
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    country: {
-        type: String,
-        required: true
-    },
-    state: {
-        type: String,
-        required: true
-    },
-    city: {
-        type: String,
-        required: true
-    },
-    address: {
-        type: String,
-        required: false,
-        default: null
-    },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  birthday: {
+    type: Date,
+    required: false,
+    default: null,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: false,
+  },
+  state: {
+    type: String,
+    required: false,
+  },
+  city: {
+    type: String,
+    required: false,
+  },
+  address: {
+    type: String,
+    required: false,
+    default: null,
+  },
 });
 
-const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 module.exports = userModel;
