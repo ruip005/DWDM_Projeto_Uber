76758f12389511cde530d29481126d8477be1b87
psvita64 <148880754+psvita64@users.noreply.github.com>
2024-01-18 17:19:44 +0000

    Add files via upload

--- ReactModule/src/pages/MyRestaurante/ProductsLista.js
+++ ReactModule/src/pages/MyRestaurante/ProductsLista.js
@@ -4,28 +4,28 @@ let ProductsLista = [
         image: 'https://i.ebayimg.com/images/g/3EkAAOSwCpRfFsNd/s-l1600.jpg',
         name: 'Fumo Vermelho',
         price: 10,
-        quantidade: 1,
+        quantity: 1,
     },
     {
         id: 2,
         image: 'https://m.media-amazon.com/images/I/51G22XSlZDL._AC_UF894,1000_QL80_.jpg',
         name: 'Fumo Azul',
         price: 20,
-        quantidade: 1,
+        quantity: 1,
     },
     {
         id: 3,
         image: 'https://m.media-amazon.com/images/I/51G8Lnr2VWL.jpg',
         name: 'Fumo Coelho',
         price: 15,
-        quantidade: 1,
+        quantity: 1,
     },
     {
         id: 4,
         image: 'https://img.fruugo.com/product/3/78/813921783_max.jpg',
         name: 'Fumo Rosa',
         price: 25,
-        quantidade: 1,
+        quantity: 1,
     },
 ];
 
